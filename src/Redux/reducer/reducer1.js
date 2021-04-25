0b015964821710da6c4b4d614122b4df3ed7fa34
tuanphan1998 <tuanphani.c.t@gmail.com>
2021-04-25 09:01:08 +0700

    okk change

--- src/Redux/reducer/reducer1.js
+++ src/Redux/reducer/reducer1.js
@@ -7,7 +7,6 @@ const initState = {
 const reducer1 = (state = initState, action) => {
     switch (action.type) {
         case 'LAY_DATA':
-        console.log(JSON.stringify(action.getitem));
         firebaseone.push(action.getitem);
         return{...state,LuuData:action.getitem}
         default:
