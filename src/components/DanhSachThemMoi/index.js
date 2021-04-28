2554fab40ed06374361a44ed51a460d8a9abdc89
tuanphan1998 <tuanphani.c.t@gmail.com>
2021-04-28 15:32:55 +0700

    ok change

--- src/components/DanhSachThemMoi/index.js
+++ src/components/DanhSachThemMoi/index.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import ReactHTMLTableToExcel from 'react-html-table-to-excel';
 import {firebaseDemo} from '../../Firebase/firebaseconnectio';
-import Search from './HienThi/DuLieuSearchItem';
+import Search from './TimKiem/Search';
 import KOKOBAYs from './HienThi/DuLieuSearch';
 
 class ListTable extends Component {
@@ -14,139 +14,139 @@ class ListTable extends Component {
         };
     }
     componentDidMount() {
-			if (JSON.parse(localStorage.getItem('dataUserNamePush')).email === 'toduyen0402@gmail.com') {
+			   if (JSON.parse(localStorage.getItem('dataUserNamePush')).email === 'toduyen0402@gmail.com') {
 
-				let ghinhandata = firebaseDemo;
-				ghinhandata.on('value', (snapshort) => {
-						var Mangs = [];
-						snapshort.forEach((element) => {
-								ghinhandata
-										.child(element.key)
-										.child('DataCard')
-										.on('value', (datas) => {
-												datas.forEach((elementChinhThuc) => {
-														const key = elementChinhThuc.key;
-														const name = elementChinhThuc
-																.val()
-																.name;
-														const id = elementChinhThuc
-																.val()
-																.id;
-														const dob = elementChinhThuc
-																.val()
-																.dob;
-														const home = elementChinhThuc
-																.val()
-																.home;
-														const address = elementChinhThuc
-																.val()
-																.address;
-														const sex = elementChinhThuc
-																.val()
-																.sex;
-														const nationality = elementChinhThuc
-																.val()
-																.nationality;
-														const doe = elementChinhThuc
-																.val()
-																.doe;
-														const ethnicity = elementChinhThuc
-																.val()
-																.ethnicity;
-														const tonhiao = elementChinhThuc
-																.val()
-																.tonhiao;
-														const ngayhethan = elementChinhThuc
-																.val()
-																.ngayhethan;
-														const noicap = elementChinhThuc
-																.val()
-																.noicap;
-														Mangs.push({
-																key: key,
-																name: name,
-																id: id,
-																dob: dob,
-																home: home,
-																address: address,
-																sex: sex,
-																nationality: nationality,
-																doe: doe,
-																ethnicity: ethnicity,
-																tonhiao: tonhiao,
-																ngayhethan: ngayhethan,
-																noicap: noicap
-														})
-												});
-										});
-						})
-						this.setState({data: Mangs})
-				})
-		} else {
+            let ghinhandata = firebaseDemo;
+            ghinhandata.on('value', (snapshort) => {
+                var Mangs = [];
+                snapshort.forEach((element) => {
+                    ghinhandata
+                        .child(element.key)
+                        .child('DataCard')
+                        .on('value', (datas) => {
+                            datas.forEach((elementChinhThuc) => {
+                                const key = elementChinhThuc.key;
+                                const name = elementChinhThuc
+                                    .val()
+                                    .name;
+                                const id = elementChinhThuc
+                                    .val()
+                                    .id;
+                                const dob = elementChinhThuc
+                                    .val()
+                                    .dob;
+                                const home = elementChinhThuc
+                                    .val()
+                                    .home;
+                                const address = elementChinhThuc
+                                    .val()
+                                    .address;
+                                const sex = elementChinhThuc
+                                    .val()
+                                    .sex;
+                                const nationality = elementChinhThuc
+                                    .val()
+                                    .nationality;
+                                const doe = elementChinhThuc
+                                    .val()
+                                    .doe;
+                                const ethnicity = elementChinhThuc
+                                    .val()
+                                    .ethnicity;
+                                const tonhiao = elementChinhThuc
+                                    .val()
+                                    .tonhiao;
+                                const ngayhethan = elementChinhThuc
+                                    .val()
+                                    .ngayhethan;
+                                const noicap = elementChinhThuc
+                                    .val()
+                                    .noicap;
+                                Mangs.push({
+                                    key: key,
+                                    name: name,
+                                    id: id,
+                                    dob: dob,
+                                    home: home,
+                                    address: address,
+                                    sex: sex,
+                                    nationality: nationality,
+                                    doe: doe,
+                                    ethnicity: ethnicity,
+                                    tonhiao: tonhiao,
+                                    ngayhethan: ngayhethan,
+                                    noicap: noicap
+                                })
+                            });
+                        });
+                })
+                this.setState({data: Mangs})
+            })
+        } else {
 
-				firebaseDemo
-						.child(JSON.parse(localStorage.getItem('dataUserNamePush')).key)
-						.child('DataCard')
-						.on('value', (datas) => {
-								var Mang = [];
-								datas.forEach(element => {
-										const key = element.key;
-										const name = element
-												.val()
-												.name;
-										const id = element
-												.val()
-												.id;
-										const dob = element
-												.val()
-												.dob;
-										const home = element
-												.val()
-												.home;
-										const address = element
-												.val()
-												.address;
-										const sex = element
-												.val()
-												.sex;
-										const nationality = element
-												.val()
-												.nationality;
-										const doe = element
-												.val()
-												.doe;
-										const ethnicity = element
-												.val()
-												.ethnicity;
-										const tonhiao = element
-												.val()
-												.tonhiao;
-										const ngayhethan = element
-												.val()
-												.ngayhethan;
-										const noicap = element
-												.val()
-												.noicap;
-										Mang.push({
-												key: key,
-												name: name,
-												id: id,
-												dob: dob,
-												home: home,
-												address: address,
-												sex: sex,
-												nationality: nationality,
-												doe: doe,
-												ethnicity: ethnicity,
-												tonhiao: tonhiao,
-												ngayhethan: ngayhethan,
-												noicap: noicap
-										})
-								});
-								this.setState({data: Mang})
-						})
+            firebaseDemo
+                .child(JSON.parse(localStorage.getItem('dataUserNamePush')).key)
+                .child('DataCard')
+                .on('value', (datas) => {
+                    var Mang = [];
+                    datas.forEach(element => {
+                        const key = element.key;
+                        const name = element
+                            .val()
+                            .name;
+                        const id = element
+                            .val()
+                            .id;
+                        const dob = element
+                            .val()
+                            .dob;
+                        const home = element
+                            .val()
+                            .home;
+                        const address = element
+                            .val()
+                            .address;
+                        const sex = element
+                            .val()
+                            .sex;
+                        const nationality = element
+                            .val()
+                            .nationality;
+                        const doe = element
+                            .val()
+                            .doe;
+                        const ethnicity = element
+                            .val()
+                            .ethnicity;
+                        const tonhiao = element
+                            .val()
+                            .tonhiao;
+                        const ngayhethan = element
+                            .val()
+                            .ngayhethan;
+                        const noicap = element
+                            .val()
+                            .noicap;
+                        Mang.push({
+                            key: key,
+                            name: name,
+                            id: id,
+                            dob: dob,
+                            home: home,
+                            address: address,
+                            sex: sex,
+                            nationality: nationality,
+                            doe: doe,
+                            ethnicity: ethnicity,
+                            tonhiao: tonhiao,
+                            ngayhethan: ngayhethan,
+                            noicap: noicap
+                        })
+                    });
+                    this.setState({data: Mang})
+                })
 
-		}
+        }
 		}
 		
 
