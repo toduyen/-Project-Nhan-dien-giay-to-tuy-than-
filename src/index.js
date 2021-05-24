33b58dba66e45bdab42aacd70d5ac5af30beb8eb
tuanphan1998 <tuanphani.c.t@gmail.com>
2021-05-24 07:07:57 +0700

    ok change

--- src/index.js
+++ src/index.js
@@ -6,9 +6,9 @@ import * as serviceWorker from './serviceWorker';
 import {Provider} from 'react-redux';
 import store1 from '../src/Redux/store';
 ReactDOM.render(<Provider store={store1}>
-	<React.StrictMode>
+
 	<App />
-	</React.StrictMode>
+	
 </Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
