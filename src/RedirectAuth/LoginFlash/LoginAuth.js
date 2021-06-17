94965ef32483e77c9673efb82911f8abbc0dfa99
tuanphan1998 <tuanphani.c.t@gmail.com>
2021-06-17 21:10:40 +0700

    main

--- src/RedirectAuth/LoginFlash/LoginAuth.js
+++ src/RedirectAuth/LoginFlash/LoginAuth.js
@@ -17,7 +17,7 @@ class LoginAuth extends Component {
           <button
             className="facebook"
             onClick={() => this.props.authenticate("Facebook")}>
-            <svg xmlns="http://www.w3.org/2000/svg" width="46" height="46" fill="currentColor" class="bi bi-door-closed" viewBox="0 0 26 16">
+            <svg xmlns="http://www.w3.org/2000/svg" width="46" height="46" fill="currentColor" className="bi bi-door-closed" viewBox="0 0 26 16">
               <path d="M3 2a1 1 0 0 1 1-1h8a1 1 0 0 1 1 1v13h1.5a.5.5 0 0 1 0 1h-13a.5.5 0 0 1 0-1H3V2zm1 13h8V2H4v13z" />
               <path d="M9 9a1 1 0 1 0 2 0 1 1 0 0 0-2 0z" />
             </svg>
